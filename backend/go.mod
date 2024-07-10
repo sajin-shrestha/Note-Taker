6d9ebfb70c68e3a8b633ecb859ec15b68c265d6b
SajinShrestha2003 <shresthasajin59@gmail.com>
2024-07-10 20:33:16 +0545

    models defined for notes

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/sajin-shrestha/Note-Taker
+
+go 1.21.1
---
+++ backend/models/models.go
@@ -0,0 +1,23 @@
+package models
+
+import (
+	"time"
+)
+
+type Account struct {
+	ID        int       `json:"id"`
+	Email     string    `json:"email"`
+	Username  string    `json:"username"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+type NoteModel struct {
+	ID          int       `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	ImageURL    string    `json:"image"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updatedAt"`
+}
